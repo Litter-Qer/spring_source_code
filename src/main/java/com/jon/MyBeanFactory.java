396e333d729f7f6d5e5b53b2e064985a0dcce347
Litter-Qer <Jonchen399@gmail.com>
2024-07-01 01:39:45 +1000

    bean factory features

--- src/main/java/com/jon/MyBeanFactory.java
+++ src/main/java/com/jon/MyBeanFactory.java
@@ -1,9 +1,9 @@
 package com.jon;
 
 import lombok.extern.log4j.Log4j2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
@@ -11,10 +11,17 @@ import org.springframework.context.annotation.AnnotationConfigUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Map;
-
 @Log4j2
 public class MyBeanFactory {
+
+    /*
+        Bean Factory will not do:
+            - add BeanFactory post processors
+            - add Bean post processors
+            - instantiate singletons
+            - interpret ${} or #{}
+     */
+
     public static void main(String[] args) {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         // definition of a bean, (class, scope, initial, deletion)
@@ -35,15 +42,28 @@ public class MyBeanFactory {
         }
 
         // get all bean post processors of beanFactory and then process
-        beanFactory.getBeansOfType(BeanFactoryPostProcessor.class).values().forEach(beanFactoryPostProcessor->{
+        beanFactory.getBeansOfType(BeanFactoryPostProcessor.class).values().forEach(beanFactoryPostProcessor -> {
             beanFactoryPostProcessor.postProcessBeanFactory(beanFactory);
         });
 
         // find some post processors who would process annotations orderly
         for (String beanDefinitionName : beanFactory.getBeanDefinitionNames()) {
-            log.info("after post process: {}", beanDefinitionName);
+            log.info("after bean factory post process: {}", beanDefinitionName);
         }
 
+        // find that bean2 is null in Bean1
+//        log.info("get bean2 --> {}", beanFactory.getBean(Bean1.class).getBean2());
+
+        // bean post processors, used to extend features of bean life cycle, such as @Autowired, @Resource...
+        // add bean post processors to bean factory
+        beanFactory.getBeansOfType(BeanPostProcessor.class).values().forEach(beanFactory::addBeanPostProcessor);
+
+        // create beans before using getBean. creates all singleton beans
+        beanFactory.preInstantiateSingletons();
+
+        log.info(">>>>>>>>>>>>>>>>>>>>>> Splitting Line >>>>>>>>>>>>>>>>>>>>>>");
+        // getBean will create a new Bean. it will be null again if Bean1 is created before this (singleton mode)
+        log.info("after bean processing, get bean2 --> {}", beanFactory.getBean(Bean1.class).getBean2());
     }
 
     @Configuration
@@ -60,11 +80,24 @@ public class MyBeanFactory {
 
     }
 
+    @Log4j2
     static class Bean1 {
-        public static final Logger log = LoggerFactory.getLogger(Bean1.class);
+        public Bean1() {
+            log.info("constructing bean1");
+        }
+
+        @Autowired
+        private Bean2 bean2;
+
+        public Bean2 getBean2() {
+            return bean2;
+        }
     }
 
+    @Log4j2
     static class Bean2 {
-        public static final Logger log = LoggerFactory.getLogger(Bean2.class);
+        public Bean2() {
+            log.info("constructing bean2");
+        }
     }
 }
