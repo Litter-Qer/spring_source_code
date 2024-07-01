9ec569088f4800fdabc010f4b6a85e0213318128
Litter-Qer <Jonchen399@gmail.com>
2024-07-01 21:48:55 +1000

    Annotation Config Application Context

--- src/main/java/com/jon/TestApplicationContext.java
+++ src/main/java/com/jon/TestApplicationContext.java
@@ -3,6 +3,10 @@ package com.jon;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
@@ -12,16 +16,26 @@ public class TestApplicationContext {
 
     public static void main(String[] args) {
 //        testClassPathXmlApplicationContext();
-        testFileSystemXmlApplicationContext();
+//        testFileSystemXmlApplicationContext();
 //        testAnnotationConfigApplicationContext();
+        testAnnotationConfigServletWebServerApplicationContext();
+    }
 
+    private static void testAnnotationConfigServletWebServerApplicationContext() {
+        AnnotationConfigServletWebServerApplicationContext context = new AnnotationConfigServletWebServerApplicationContext();
     }
 
     /**
      *
      */
     private static void testAnnotationConfigApplicationContext() {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestConfig.class);
+        for (String name : context.getBeanDefinitionNames()) {
+            log.info("Bean name: {}", name);
+        }
 
+        Bean1 bean1 = context.getBean(Bean2.class).getBean1();
+        log.info("Bean1 ---> {}", bean1);
     }
 
     /**
@@ -58,6 +72,21 @@ public class TestApplicationContext {
 
         Bean1 bean1 = context.getBean(Bean2.class).getBean1();
         log.info("Bean1 ---> {}", bean1);
+    }
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public Bean1 bean1() {
+            return new Bean1();
+        }
+
+        @Bean
+        public Bean2 bean2(Bean1 bean1) {
+            Bean2 bean2 = new Bean2();
+            bean2.setBean1(bean1);
+            return bean2;
+        }
     }
 
     static class Bean1 {
