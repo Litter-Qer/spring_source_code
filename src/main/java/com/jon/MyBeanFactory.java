06685899defe5450e21508e7219eb993431c2113
Litter-Qer <Jonchen399@gmail.com>
2024-07-01 21:04:01 +1000

    Bean post processor order

--- src/main/java/com/jon/MyBeanFactory.java
+++ src/main/java/com/jon/MyBeanFactory.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.AnnotationConfigUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.Resource;
+
 @Log4j2
 public class MyBeanFactory {
 
@@ -64,6 +66,10 @@ public class MyBeanFactory {
         log.info(">>>>>>>>>>>>>>>>>>>>>> Splitting Line >>>>>>>>>>>>>>>>>>>>>>");
         // getBean will create a new Bean. it will be null again if Bean1 is created before this (singleton mode)
         log.info("after bean processing, get bean2 --> {}", beanFactory.getBean(Bean1.class).getBean2());
+
+        // bean post processor has order, so bean 3 is injected before bean 4
+        log.info("after bean processing, get bean1 --> {}", beanFactory.getBean(Bean1.class).getInter());
+
     }
 
     @Configuration
@@ -78,6 +84,15 @@ public class MyBeanFactory {
             return new Bean2();
         }
 
+        @Bean
+        public Bean3 bean3() {
+            return new Bean3();
+        }
+
+        @Bean
+        public Bean4 bean4() {
+            return new Bean4();
+        }
     }
 
     @Log4j2
@@ -92,12 +107,35 @@ public class MyBeanFactory {
         public Bean2 getBean2() {
             return bean2;
         }
+
+        @Autowired
+        @Resource(name = "bean4")
+        private Inter bean3;
+
+        public Inter getInter() {
+            return bean3;
+        }
     }
 
     @Log4j2
     static class Bean2 {
         public Bean2() {
             log.info("constructing bean2");
+        }
+    }
+
+    interface Inter {
+    }
+
+    static class Bean3 implements Inter {
+        public Bean3() {
+            log.info("constructing bean3");
+        }
+    }
+
+    static class Bean4 implements Inter {
+        public Bean4() {
+            log.info("constructing bean4");
         }
     }
 }
